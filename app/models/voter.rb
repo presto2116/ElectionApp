150e6fc1c7ca9d5f3e5ab7aafb5fc0515ca6244e
presto2116 <preston.straight@gmail.com>
2015-10-01 15:00:05 -0400

    added validation for first_name and last_name

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -1,6 +1,8 @@
 class Voter < ActiveRecord::Base
 	#Validations for required fields
 		validates :first_name, :last_name, :address, :city, :state, :zip, presence: true
+	#validation for no spaces in first and last name
+		validates :first_name, :last_name, format: { without: /\s/, message: ": No Spaces allowed"}
 	#validation for state code must be 2 characters i.e. DC
 		validates :state, length: {is: 2 }
 	#validation for zip must be a number & 5 characters long
