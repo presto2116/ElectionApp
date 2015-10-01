4cb4170f899fcfd4b2d4a268d6ecd8c4bab359de
presto2116 <preston.straight@gmail.com>
2015-10-01 15:25:17 -0400

    added messages to validatons

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -1,8 +1,10 @@
 class Voter < ActiveRecord::Base
 	#Validations for required fields
 		validates :first_name, :last_name, :address, :city, :state, :zip, presence: true
+	#validation for no spaces in first and last name
+		validates :first_name, :last_name, format: { without: /\s/, message: ": No Spaces allowed"}
 	#validation for state code must be 2 characters i.e. DC
-		validates :state, length: {is: 2 }
+		validates :state, length: {is: 2, message: ": Use abbreviation ie. DC" }
 	#validation for zip must be a number & 5 characters long
-		validates :zip, numericality: true, length: { is: 5 }
+		validates :zip, numericality: true, length: { is: 5, message: ": Zip Code must be 5 numbers" }
 end
